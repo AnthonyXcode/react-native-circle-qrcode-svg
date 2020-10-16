20c2f678557c29af46f1d8be85f24e4a8a350789
Anthony Chan <anthony.chan@gogotech.hk>
2020-10-16 12:25:59 +0800

    simplify the path

--- src/transformMatrixIntoPath.js
+++ src/transformMatrixIntoPath.js
@@ -1,22 +1,11 @@
-export default (matrix, size) => {
+export default (matrix, size, isCircle) => {
   const cellSize = size / matrix.length
   let path = ''
   matrix.forEach((row, i) => {
-    let needDraw = false
     row.forEach((column, j) => {
       if (column) {
-        if (!needDraw) {
-          path += `M${cellSize * j} ${cellSize / 2 + cellSize * i} `
-          needDraw = true
-        }
-        if (j === matrix.length - 1) {
-          path += `L${cellSize * (j + 1)} ${cellSize / 2 + cellSize * i} `
-        }
-      } else {
-        if (needDraw) {
-          path += `L${cellSize * j} ${cellSize / 2 + cellSize * i} `
-          needDraw = false
-        }
+        path += `M${cellSize * j} ${cellSize / 2 + cellSize * i} `
+        path += `L${cellSize * (j + 1)} ${cellSize / 2 + cellSize * i} `
       }
     })
   })
