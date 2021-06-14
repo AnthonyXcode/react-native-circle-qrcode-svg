31bdd0ce2a3e091db616fff6d6b5d60225654928
Anthony Chan <anthony.chan@gogotech.hk>
2021-06-14 16:54:29 +0800

    fix circle QR code decoration problem

--- src/transformMatrixIntoPath.js
+++ src/transformMatrixIntoPath.js
@@ -1,6 +1,7 @@
 export default (matrix, size, isCircle) => {
+  console.log(size)
   const cellSize = (isCircle ? (size * Math.cos(Math.PI / 4) - 10) : size) / matrix.length
-  const startingPoint = isCircle ? (size - size * Math.cos(Math.PI / 4) + 10) / 2 : 0
+  const startingPoint = isCircle ? (size - size * Math.cos(Math.PI / 4) + cellSize) / 2 : 0
   let path = ''
   matrix.forEach((row, i) => {
     row.forEach((column, j) => {
@@ -21,8 +22,8 @@ export default (matrix, size, isCircle) => {
         j += 1
       }
       const shouldDraw = Math.round(Math.random()) === 1
-      const isOutsideX = (i * cellSize < startingPoint)
-      const isOutsideY = (j * cellSize < startingPoint) || (j * cellSize > size - startingPoint)
+      const isOutsideX = (i * cellSize < startingPoint - cellSize * 2)
+      const isOutsideY = (j * cellSize < startingPoint - cellSize * 2) || (j * cellSize > size - startingPoint)
       if (shouldDraw && isOutsideX) {
         path += `M${i * cellSize + offset} ${cellSize / 2 + j * cellSize + offset} `
         path += `L${(i + 1) * cellSize + offset} ${cellSize / 2 + j * cellSize + offset} `
